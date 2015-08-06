1737b375c50e5d2a8a17a2964a421eed086338ff
GreyKnight <tinyplasticgreyknight@yahoo.com>
2015-08-06 16:45:21 +0100

    initial

---
+++ Converter.cs
@@ -0,0 +1,8 @@
+﻿namespace pocketmodise {
+    using System.Windows.Input;
+
+    public class Converter {
+        public ICommand OpenInputCommand { get; set; }
+        public ICommand RenderOutputCommand { get; set; }
+    }
+}
---
+++ PocketmodiserWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace pocketmodise {
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for PocketmodiserWindow.xaml
+    /// </summary>
+    public partial class PocketmodiserWindow : Window {
+        private readonly Converter ConverterBinder;
+
+        public PocketmodiserWindow() {
+            InitializeComponent();
+            ConverterBinder = new Converter();
+            DataContext = ConverterBinder;
+        }
+    }
+}
